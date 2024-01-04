e416b58df90bfd38e92610dc8bd19194e04495e4
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-01-05 06:06:09 +0900

    device サインアップ完了

--- db/migrate/20231219210932_create_residence.rb
+++
@@ -1,7 +0,0 @@
-class CreateResidence < ActiveRecord::Migration[7.1]
-  def change
-    create_table :residences do |t|
-      t.integer :residence_name, null: false, default: 0
-    end
-  end
-end
--- db/migrate/20231219211021_create_sensation.rb
+++
@@ -1,7 +0,0 @@
-class CreateSensation < ActiveRecord::Migration[7.1]
-  def change
-    create_table :sensations do |t|
-      t.integer :sensation_name, null: false, default: 0
-    end
-  end
-end
