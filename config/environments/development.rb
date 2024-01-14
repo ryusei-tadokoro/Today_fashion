0eb152912adfd5524e110d4ba810ac7fc7909b31
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-01-14 16:06:54 +0900

    お問い合わせメール、遷移までは完了したがメールが送れない

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,6 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
@@ -46,14 +44,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # Gmail の場合
   config.action_mailer.smtp_settings = {
-  :enable_starttls_auto => true,
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :domain => 'smtp.gmail.com',
-  :user_name => ENV['GMAIL_ADDRESS'],
-  :password => ENV['GMAIL_PASSWORD'],
-  :authentication => 'login'
-}
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'gmail.com',
+    :user_name => ENV['GMAIL_ADDRESS'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :authentication => 'plain'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
