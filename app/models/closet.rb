439474f95115c530edf611967ab12fac412cd516
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-01-21 16:02:22 +0900

    サブカテゴリー実装前

--- app/models/closet.rb
+++ app/models/closet.rb
@@ -1,6 +1,7 @@
 class Closet < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :users, foreign_key: :user_id
+  has_many :categories
 
   validates :name, presence: true
   validates :category, presence: true
