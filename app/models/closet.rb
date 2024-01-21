59196f9ebf277345cc8e896a5b291b163caf8d7a
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-01-21 19:55:54 +0900

    服登録画面ほぼ完了

--- app/models/closet.rb
+++ app/models/closet.rb
@@ -1,11 +1,9 @@
 class Closet < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
-  belongs_to :category
-  belongs_to :subcategory
-  has_many :subcategories, through: :category
+  belongs_to :category, optional: true
+  belongs_to :subcategory, optional: true
 
   validates :name, presence: true
   validates :category, presence: true
-  validates :subcategory, presence: true
 end
