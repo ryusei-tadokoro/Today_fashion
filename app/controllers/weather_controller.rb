19bd9c1e08f7e671ac5bd616340db59d7a1dd140
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-01-27 11:48:51 +0900

    トップページ作成中

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -8,6 +8,42 @@ class WeatherController < ApplicationController
   before_action :load_prefecture_data, only: [:index]
 
   def index
+    # デフォルトの都市を東京に設定
+    default_city = 'Tokyo'
+
+    # WeatherServiceのインスタンスを生成し、fetch_weatherメソッドを呼び出して天気情報を取得
+    response = WeatherService.new(default_city).fetch_weather
+
+    # HTTPレスポンスが成功かどうかをチェック
+    if response.success?
+      # HTTParty からのレスポンスをパースして、Rubyのハッシュ形式のデータ構造（weather_data）に変換
+      weather_data = response.parsed_response
+      @default_weather = {
+        # ... (必要な情報を追加)
+
+        # 都市名（東京都新宿区）
+        name: "#{weather_data['name']} #{weather_data['sys']['country']}",
+        # 現在の温度
+        temp_celsius: kelvin_to_celsius(weather_data['main']['temp']).round(2),
+        # 最高気温
+        temp_max_celsius: kelvin_to_celsius(weather_data['main']['temp_max']).round(2),
+        # 最低気温
+        temp_min_celsius: kelvin_to_celsius(weather_data['main']['temp_min']).round(2),
+        # 湿度
+        humidity: weather_data['main']['humidity'],
+        # 降水確率（APIには降水確率の直接の情報が含まれていない場合もあります）
+        precipitation_probability: weather_data.dig('rain', '1h') || 'N/A',
+        # 風速
+        wind_speed: weather_data['wind']['speed'],
+        # 天気
+        description: weather_data['weather'][0]['description'],
+        # 今日のひとこと（適切な情報がAPIに含まれていれば使用）
+        today_message: 'デフォルトの天気情報です。'
+      }
+    else
+      # 失敗した場合はエラーメッセージを表示
+      flash.now[:alert] = "天気情報の取得に失敗しました。"
+    end
   end
 
   def show
@@ -54,6 +90,10 @@ class WeatherController < ApplicationController
     @prefecture_data = CSV.read(csv_path, headers: true).map { |row| [row['prefecture'], row['english']] }
   end
 
+  def fetch_weather_by_city_id(city_id)
+    WeatherService.new(city_id, use_city_id: true).fetch_weather
+  end
+
 
   # ケルビン単位の温度を摂氏単位に変換するkelvin_to_celsiusメソッドを定義
   def kelvin_to_celsius(kelvin)
@@ -71,7 +111,11 @@ class WeatherService
     # 環境変数からOpenWeatherMapのAPIキーを取得
     api_key = ENV['OPENWEATHERMAP_API_KEY']
     # APIリクエストのためのオプションを設定
-    @options = { query: { q: "#{city},jp", appid: api_key, lang: 'ja' } }
+    if use_city_id
+      @options = { query: { id: city, appid: api_key, lang: 'ja' } }
+    else
+      @options = { query: { q: "#{city},jp", appid: api_key, lang: 'ja' } }
+    end
   end
 
   def fetch_weather
