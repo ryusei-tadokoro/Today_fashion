0e81b369397b14680b4f60cbd3bc8290c5f76b00
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-01-27 13:24:05 +0900

    index 途中

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_21_104959) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_27_040605) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -52,6 +52,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_21_104959) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_subcategories_on_category_id"
+  end
+
+  create_table "today_messages", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
