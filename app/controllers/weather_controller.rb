1499af64e8829c2cb7d5988dba4373820c4aef54
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-01-28 14:52:55 +0900

    トップページほぼ完了,あとはアイコン表示等

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,6 +1,4 @@
-# gem httpartyを読み込む
 require 'httparty'
-# URIモジュールを読み込む（URLのエンコードや解析に使われる）
 require 'uri'
 
 class WeatherController < ApplicationController
@@ -8,78 +6,22 @@ class WeatherController < ApplicationController
   before_action :load_prefecture_data, only: [:index]
 
   def index
-   # ユーザーの地域を取得
     user_prefecture_id = current_user&.prefecture_id
-
-    # ユーザーの地域が設定されている場合はそれを使用し、そうでなければデフォルトの都市を東京に設定
+    user_second_prefecture_id = current_user&.second_prefecture_id
     default_city = user_prefecture_id.present? ? Prefecture.find(user_prefecture_id).name : 'Tokyo'
-
-    # WeatherServiceのインスタンスを生成し、fetch_weatherメソッドを呼び出して天気情報を取得
-    response = WeatherService.new(default_city).fetch_weather
-    if response.success?
-      # HTTParty からのレスポンスをパースして、Rubyのハッシュ形式のデータ構造（weather_data）に変換
-      weather_data = response.parsed_response
-      @default_weather = {
-        # 都市名（東京都）
-        name: "#{weather_data['name']} #{weather_data['sys']['country']}",
-        # 現在の温度
-        temp_celsius: kelvin_to_celsius(weather_data['main']['temp']).round(2),
-        # 最高気温
-        temp_max_celsius: kelvin_to_celsius(weather_data['main']['temp_max']).round(2),
-        # 最低気温
-        temp_min_celsius: kelvin_to_celsius(weather_data['main']['temp_min']).round(2),
-        # 湿度
-        humidity: weather_data['main']['humidity'],
-        # 降水確率（APIには降水確率の直接の情報が含まれていない場合もあります）
-        precipitation_probability: weather_data.dig('rain', '1h') || 'N/A',
-        # 風速
-        wind_speed: weather_data['wind']['speed'],
-        # 天気
-        description: weather_data['weather'][0]['description'],
-        # 今日のひとこと（適切な情報がAPIに含まれていれば使用）
-        today_message: random_today_message(weather_data)
-      }
-    else
-      # 失敗した場合はエラーメッセージを表示
-      flash.now[:alert] = "天気情報の取得に失敗しました。"
-    end
+    @weather_data = fetch_weather_data(default_city)
+    second_city = user_second_prefecture_id.present? ? Prefecture.find(user_second_prefecture_id).name : 'Osaka'
+    @second_weather_data = fetch_weather_data(second_city)
+    flash.now[:alert] = "天気情報の取得に失敗しました。" unless @weather_data || @second_weather_data
   end
 
   def show
-    # WeatherServiceのインスタンスを生成し、fetch_weatherメソッドを呼び出して天気情報を取得
-    response = WeatherService.new(params[:city]).fetch_weather
-    # HTTPレスポンスが成功かどうかをチェック
-    if response.success?
-      # HTTParty からのレスポンスをパースして、Rubyのハッシュ形式のデータ構造（weather_data）に変換
-      weather_data = response.parsed_response
-      rainfall = weather_data['rain']['3h'] if weather_data['rain']
-      @weather = {
-        # 取得したweather_dataハッシュから特定の気象データを取り出し、適切な形式に変換
-        # 都市の天気
-        name: weather_data["name"],
-        # 現在の温度
-        temp_celsius: kelvin_to_celsius(weather_data["main"]["temp"]).round(2),
-        # 体感温度
-        feels_like_celsius: kelvin_to_celsius(weather_data["main"]["feels_like"]).round(2),
-        # 最低気温
-        temp_min_celsius: kelvin_to_celsius(weather_data["main"]["temp_min"]).round(2),
-        # 最高気温
-        temp_max_celsius: kelvin_to_celsius(weather_data["main"]["temp_max"]).round(2),
-        # 湿度
-        humidity: weather_data["main"]["humidity"],
-        # 風速
-        wind_speed: weather_data["wind"]["speed"],
-        # 天気
-        description: weather_data["weather"][0]["description"],
-        rainfall: rainfall
-      }
-      # 5日間の天気予報を取得
-      response_forecast = WeatherService.new(params[:city]).fetch_weather_forecast
-      if response_forecast.success?
-        @weather_data = response_forecast.parsed_response
-      end
+    @weather_data = fetch_weather_data(params[:city])
+    if @weather_data
+      rainfall = @weather_data['rain']['3h'] if @weather_data['rain']
+      @weather = extract_weather_data(@weather_data, rainfall)
+      fetch_and_update_weather_forecast(params[:city])
     else
-      # 成功でない場合、ユーザーをindexアクションにリダイレクトし、エラーメッセージを表示
       redirect_to action: :index, alert: "天気情報の取得に失敗しました。"
     end
   end
@@ -87,7 +29,6 @@ class WeatherController < ApplicationController
   private
 
   def validate_city
-    # 都市名が空であれば、ユーザーをindexアクションにリダイレクトし、警告メッセージを表示
     redirect_to(action: :index, alert: "都市名を入力してください。") if params[:city].blank?
   end
 
@@ -96,25 +37,32 @@ class WeatherController < ApplicationController
     @prefecture_data = CSV.read(csv_path, headers: true).map { |row| [row['prefecture'], row['english']] }
   end
 
-  def fetch_weather_by_city_id(city_id)
-    WeatherService.new(city_id, use_city_id: true).fetch_weather
+  def fetch_weather_data(city)
+    response = WeatherService.new(city).fetch_weather
+    response.success? ? response.parsed_response : nil
   end
 
+  def extract_weather_data(weather_data, rainfall)
+    {
+      name: weather_data["name"],
+      temp_celsius: kelvin_to_celsius(weather_data["main"]["temp"]).round(1),
+      feels_like_celsius: kelvin_to_celsius(weather_data["main"]["feels_like"]).round(1),
+      temp_min_celsius: kelvin_to_celsius(weather_data["main"]["temp_min"]).round(1),
+      temp_max_celsius: kelvin_to_celsius(weather_data["main"]["temp_max"]).round(1),
+      humidity: weather_data["main"]["humidity"],
+      wind_speed: weather_data["wind"]["speed"],
+      description: weather_data["weather"][0]["description"],
+      rainfall: rainfall
+    }
+  end
 
-  # ケルビン単位の温度を摂氏単位に変換するkelvin_to_celsiusメソッドを定義
+  def fetch_and_update_weather_forecast(city)
+    response_forecast = WeatherService.new(city).fetch_weather_forecast
+    @weather_data = response_forecast.parsed_response if response_forecast.success?
+  end
+
   def kelvin_to_celsius(kelvin)
-    super(kelvin)
-  end
-
-  def random_today_message(weather_data)
-    return '' unless weather_data
-
-    if weather_data[:rainfall].present?
-      return "今日は傘が必要なので持っていってください。"
-    end
-
-    random_message = TodayMessage.order('RANDOM()').first
-    random_message&.content || ''
+    kelvin - 273.15
   end
 end
 
@@ -122,15 +70,9 @@ class WeatherService
   include HTTParty
   base_uri 'api.openweathermap.org'
 
-  def initialize(city, options = {})
-    # 環境変数からOpenWeatherMapのAPIキーを取得
-    api_key = ENV['OPENWEATHERMAP_API_KEY']
-    # APIリクエストのためのオプションを設定
-    if options[:use_city_id]
-      @options = { query: { id: city, appid: api_key, lang: 'ja' } }
-    else
-      @options = { query: { q: "#{city},jp", appid: api_key, lang: 'ja' } }
-    end
+  def initialize(city)
+    @api_key = ENV['OPENWEATHERMAP_API_KEY']
+    @options = { query: { q: "#{city},jp", appid: @api_key, lang: 'ja' } }
   end
 
   def fetch_weather
@@ -139,27 +81,21 @@ class WeatherService
 
   def fetch_weather_forecast
     response = self.class.get("/data/2.5/forecast", @options)
-    if response.success?
-      weather_forecast_data = response.parsed_response
-      # 5日間の予報データのリストを取得
-      forecasts = weather_forecast_data['list']
-
-      # 各予報データの温度を摂氏に変換
-      forecasts.each do |forecast|
-        forecast['main']['temp'] = kelvin_to_celsius(forecast['main']['temp']).round(1)
-        forecast['main']['feels_like'] = kelvin_to_celsius(forecast['main']['feels_like']).round(1)
-        forecast['main']['temp_min'] = kelvin_to_celsius(forecast['main']['temp_min']).round(1)
-        forecast['main']['temp_max'] = kelvin_to_celsius(forecast['main']['temp_max']).round(1)
-      end
-
-      @weather_data = weather_forecast_data
-    else
-      flash.now[:alert] = "5日間の天気予報の取得に失敗しました。"
-    end
+    update_forecast_temperatures(response.parsed_response) if response.success?
     response
   end
+  
 
   private
+
+  def update_forecast_temperatures(weather_forecast_data)
+    weather_forecast_data['list'].each do |forecast|
+      forecast['main']['temp'] = kelvin_to_celsius(forecast['main']['temp']).round(1)
+      forecast['main']['feels_like'] = kelvin_to_celsius(forecast['main']['feels_like']).round(1)
+      forecast['main']['temp_min'] = kelvin_to_celsius(forecast['main']['temp_min']).round(1)
+      forecast['main']['temp_max'] = kelvin_to_celsius(forecast['main']['temp_max']).round(1)
+    end
+  end
 
   def kelvin_to_celsius(kelvin)
     kelvin - 273.15
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,114 @@
 module ApplicationHelper
+  def kelvin_to_celsius(kelvin)
+    kelvin - 273.15
+  end
+
+  def dress_code_suggestion(temperature, constitution_id)
+    case constitution_id
+    when 1 # 暑がり
+        if temperature < 3
+            '厳しい寒さ,ダウンジャケットは必須！！'
+        elsif temperature >= 3 && temperature < 7
+            'とても寒い,今日はマフラー手袋などで寒さ対策必要'
+        elsif temperature >= 7 && temperature < 11
+            '寒い,コートを着ないと結構寒いわ'
+        elsif temperature >= 11 && temperature < 15
+            'じんわり冷える寒さ,薄手のアウターがちょうどいいかも'
+        elsif temperature >= 15 && temperature < 18
+            '程よい気温,カーディガンなど薄手のものがちょうどいいかも'
+        elsif temperature >= 18 && temperature < 22
+            '昼間の気温は快適,一応薄手のカーディガンやパーカーがあるといいかも'
+        elsif temperature >= 22 && temperature < 26
+            'じんわり暑い,薄手の長袖がちょうどいいんじゃ'
+        elsif temperature >= 26 && temperature < 31
+            'とても暑い,半袖で十分'
+        else
+            '危険な暑さ,ノースリーブや短パンでも暑いから,熱中症に気をつけて！!'
+        end
+    when 2 # やや暑がり
+        if temperature < 4
+          '厳しい寒さ,ダウンジャケットは必須！！'
+        elsif temperature >= 4 && temperature < 8
+          'とても寒い,今日はマフラー手袋などで寒さ対策が必要'
+        elsif temperature >= 8 && temperature < 12
+          '寒い,コートを着ないと結構寒いわ'
+        elsif temperature >= 12 && temperature < 16
+          'じんわり冷える寒さ,薄手のアウターがちょうどいいかも'
+        elsif temperature >= 16 && temperature < 19
+          '程よい気温,カーディガンなど薄手のものがちょうどいいかも'
+        elsif temperature >= 19 && temperature < 23
+          '昼間の気温は快適,一応薄手のカーディガンやパーカーがあるといいかも'
+        elsif temperature >= 23 && temperature < 27
+          'じんわり暑い,薄手の長袖がちょうどいいんじゃ'
+        elsif temperature >= 27 && temperature < 32
+          'とても暑い,半袖で十分'
+        else
+          '危険な暑さ,ノースリーブや短パンでも暑いから,熱中症に気をつけて！!'
+        end      
+    when 3 # 標準
+        if temperature < 5
+            '厳しい寒さ,ダウンジャケットは必須！！'
+        elsif temperature >= 5 && temperature < 9
+            'とても寒い,今日はマフラー手袋などで寒さ対策必要'
+        elsif temperature >= 9 && temperature < 13
+            '寒い,コートを着ないと結構寒いわ'
+        elsif temperature >= 13 && temperature < 17
+            'じんわり冷える寒さ,薄手のアウターがちょうどいいかも'
+        elsif temperature >= 17 && temperature < 20
+            '程よい気温,カーディガンなど薄手のものがちょうどいいかも'
+        elsif temperature >= 20 && temperature < 24
+            '昼間の気温は快適,一応薄手のカーディガンやパーカーがあるといいかも'
+        elsif temperature >= 24 && temperature < 28
+            'じんわり暑い,薄手の長袖がちょうどいいんじゃ'
+        elsif temperature >= 28 && temperature < 33
+            'とても暑い,半袖で十分'
+        else
+            '危険な暑さ,ノースリーブや短パンでも暑いから,熱中症に気をつけて！!'
+        end
+    when 4 # やや寒がり
+        if temperature < 6
+          '厳しい寒さ,ダウンジャケットは必須！！'
+        elsif temperature >= 6 && temperature < 10
+          'とても寒い,今日はマフラー手袋などで寒さ対策が必要'
+        elsif temperature >= 10 && temperature < 14
+          '寒い,コートを着ないと結構寒いわ'
+        elsif temperature >= 14 && temperature < 18
+          'じんわり冷える寒さ,薄手のアウターがちょうどいいかも'
+        elsif temperature >= 18 && temperature < 21
+          '程よい気温,カーディガンなど薄手のものがちょうどいいかも'
+        elsif temperature >= 21 && temperature < 25
+          '昼間の気温は快適,一応薄手のカーディガンやパーカーがあるといいかも'
+        elsif temperature >= 25 && temperature < 29
+          'じんわり暑い,薄手の長袖がちょうどいいんじゃ'
+        elsif temperature >= 29 && temperature < 34
+          'とても暑い,半袖で十分'
+        else
+          '危険な暑さ,ノースリーブや短パンでも暑いから,熱中症に気をつけて！!'
+        end
+      
+    when 5 # 寒がり
+        if temperature < 7
+          '厳しい寒さ,ダウンジャケットは必須！！'
+        elsif temperature >= 7 && temperature < 11
+          'とても寒い,今日はマフラー手袋などで寒さ対策が必要'
+        elsif temperature >= 11 && temperature < 15
+          '寒い,コートを着ないと結構寒いわ'
+        elsif temperature >= 15 && temperature < 18
+          'じんわり冷える寒さ,薄手のアウターがちょうどいいかも'
+        elsif temperature >= 18 && temperature < 21
+          '程よい気温,カーディガンなど薄手のものがちょうどいいかも'
+        elsif temperature >= 21 && temperature < 25
+          '昼間の気温は快適,一応薄手のカーディガンやパーカーがあるといいかも'
+        elsif temperature >= 25 && temperature < 30
+          'じんわり暑い,薄手の長袖がちょうどいいんじゃ'
+        elsif temperature >= 30 && temperature < 35
+          'とても暑い,半袖で十分'
+        else
+          '危険な暑さ,ノースリーブや短パンでも暑いから,熱中症に気をつけて！!'
+        end
+      
+    else
+      '体質に合わせた服装の提案ができません。'
+    end
+  end  
 end
