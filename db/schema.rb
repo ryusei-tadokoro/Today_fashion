da9260ad5f071c41651790da4dd5e05648507d7b
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-01-28 22:24:57 +0900

    服装に温度を持たせたが、失敗した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_27_040605) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_28_130443) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_27_040605) do
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "temperature_range"
     t.index ["category_id"], name: "index_subcategories_on_category_id"
   end
 
