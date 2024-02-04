859d2b4883da3c55aec20fd69bafe9773d6f6ab8
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-02-05 03:40:36 +0900

    先にLineメッセージを導入する

---
+++ main.rb
@@ -0,0 +1,17 @@
+require "google/cloud/vision/v1"
+
+# Google Cloud Vision API のクライアントを初期化
+image_annotator = Google::Cloud::Vision::V1::ImageAnnotator::Client.new
+
+# 画像ファイルへのパス
+file_path = "sanga.jpg"
+
+# コード内の「labelAnnotations」を「label_annotations」に修正
+response = image_annotator.label_detection(image: file_path)
+
+# 抽出されたテキストを表示
+response.responses.each do |res|
+  res.label_annotations.each do |label|
+    puts "名前: #{label.description}"
+  end
+end
