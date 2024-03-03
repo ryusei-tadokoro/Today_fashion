32c7d508cc9a2bc9103d1b1cac09d10f6152c2a9
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-03-03 16:15:37 +0900

    このバージョンでは自動画像分析ができなかった

--- db/schema.rb
+++ db/schema.rb
@@ -10,82 +10,81 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_204_010_912) do
-  create_table 'categories', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema[7.1].define(version: 2024_02_04_010912) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'closets', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'category_id', null: false
-    t.integer 'subcategory_id', null: false
-    t.date 'purchase_date'
-    t.string 'size'
-    t.string 'color'
-    t.string 'purchase_location'
-    t.decimal 'price'
-    t.integer 'usage_frequency'
-    t.string 'season'
-    t.text 'other_comments'
-    t.string 'image'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id'
-    t.date 'last_worn_on'
-    t.index ['category_id'], name: 'index_closets_on_category_id'
-    t.index ['subcategory_id'], name: 'index_closets_on_subcategory_id'
+  create_table "closets", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id", null: false
+    t.integer "subcategory_id", null: false
+    t.date "purchase_date"
+    t.string "size"
+    t.string "color"
+    t.string "purchase_location"
+    t.decimal "price"
+    t.integer "usage_frequency"
+    t.string "season"
+    t.text "other_comments"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.date "last_worn_on"
+    t.index ["category_id"], name: "index_closets_on_category_id"
+    t.index ["subcategory_id"], name: "index_closets_on_subcategory_id"
   end
 
-  create_table 'contacts', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'email', null: false
-    t.string 'subject', null: false
-    t.text 'message', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "contacts", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "subject", null: false
+    t.text "message", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'subcategories', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'category_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'temperature_range'
-    t.index ['category_id'], name: 'index_subcategories_on_category_id'
+  create_table "subcategories", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_subcategories_on_category_id"
   end
 
-  create_table 'today_messages', force: :cascade do |t|
-    t.string 'content'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "today_messages", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string 'unconfirmed_email'
-    t.integer 'prefecture_id'
-    t.integer 'constitution_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'provider'
-    t.string 'uid'
-    t.string 'name', null: false
-    t.integer 'second_prefecture_id'
-    t.string 'image'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "prefecture_id"
+    t.integer "constitution_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name", null: false
+    t.integer "second_prefecture_id"
+    t.string "image"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'closets', 'categories'
-  add_foreign_key 'closets', 'subcategories'
-  add_foreign_key 'subcategories', 'categories'
+  add_foreign_key "closets", "categories"
+  add_foreign_key "closets", "subcategories"
+  add_foreign_key "subcategories", "categories"
 end
