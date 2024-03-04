8c317efa9d99fc4908a4e47705ad1fed9cfa4553
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-03-05 05:56:11 +0900

    application.jsを一部変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,16 +14,13 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   function previewImage(input) {
-    var preview = document.getElementById('preview-image');
-    preview.innerHTML = '';
-
     if (input.files && input.files[0]) {
       var reader = new FileReader();
-
+      
       reader.onload = function (e) {
-        preview.src = e.target.result;
+        $('#preview-image').attr('src', e.target.result);
       }
-
+      
       reader.readAsDataURL(input.files[0]);
     }
   }
