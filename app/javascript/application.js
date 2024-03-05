ab8d3881bfcf02a41289f6767f9f4b696b711bd0
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-03-06 07:13:44 +0900

    java改変

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,33 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+// TurboとStimulusをImportmapからimportする
+import "@hotwired/turbo-rails";
+import { Application } from "@hotwired/stimulus";
+import controllers from "controllers";
+
+// Stimulusの設定
+const application = Application.start();
+application.load(definitionsFromContext(controllers));
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var flashNotice = document.querySelector(".alert");
+
+  if (flashNotice) {
+    alert(flashNotice.textContent.trim());
+  }
+
+  $(document).on('change', '#user_image, #image-upload', function() {
+    previewImage(this);
+  });
+
+  function previewImage(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      
+      reader.onload = function (e) {
+        $('#preview-image').attr('src', e.target.result);
+      }
+      
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+});
