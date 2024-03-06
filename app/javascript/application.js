b31e5f839442084503ecc7b8710f1f8def814b63
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-03-07 07:06:51 +0900

    自動分析も組み込めた

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,10 +9,34 @@ document.addEventListener("DOMContentLoaded", function() {
     alert(flashNotice.textContent.trim());
   }
 
-  $(document).on('change', '#user_image, #image-upload', function() {
-    previewImage(this);
-  });
+  // vision_api.jsのコードをここに追加
+  if (document.URL.match(/new/)) { // URLが適切なパスにマッチするか確認
+    $(document).on('change', 'input[type="file"]', function(e) {
+      e.preventDefault();
+      var formData = new FormData();
+      formData.append("image", e.target.files[0]);
+      $.ajax({
+        type: 'POST',
+        url: '/vision_api/upload', // 適切なエンドポイントに変更
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        headers: {
+            'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+          }
+      })
+      .done(function(data){
+        $('#closet_name').val(data.name); // フォームフィールドにラベル名を設定
+        $('#closet_name').css('background-color', 'lightgreen');
+      })
+      .fail(function(){
+        alert('画像の読み込みに失敗しました');
+      });
+    });
+  }
 
+  // プレビュー画像を表示する関数
   function previewImage(input) {
     if (input.files && input.files[0]) {
       var reader = new FileReader();
@@ -24,4 +48,8 @@ document.addEventListener("DOMContentLoaded", function() {
       reader.readAsDataURL(input.files[0]);
     }
   }
+
+  $(document).on('change', '#user_image, #image-upload', function() {
+    previewImage(this);
+  });
 });
