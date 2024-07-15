c3fa04e0576dc73fc1c36b8c2904e4bcd47088c9
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-07-15 21:57:41 +0900

    error完了

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -21,7 +21,7 @@ module Users
         if @step == 'step2'
           @user.save
           sign_in(@user)
-          redirect_to root_path, notice: 'Successfully registered.'
+          redirect_to root_path, notice: 'アカウント登録完了しました。さあ！始めよう!!'
         else
           redirect_to new_user_registration_step_path(step: next_step(@step))
         end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,3 @@
-// app/javascript/application.js
-
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import Rails from "@rails/ujs";
@@ -8,19 +6,19 @@ import "bootstrap";
 import "./scripts"; // scripts.js
 import "./closet_form"; // closet_form.js
 
-Rails.start();
+if (typeof Rails !== 'undefined') {
+  Rails.start();
+}
 
 window.$ = $;
 window.jQuery = $;
 
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function() {
   var flashNotice = document.querySelector(".alert");
-
   if (flashNotice) {
     alert(flashNotice.textContent.trim());
     flashNotice.remove(); // アラートが表示された後に要素を削除
   }
-
   if (document.URL.match(/new/)) {
     import(/* webpackChunkName: "vision_api" */ './vision_api').then(module => {
       const visionAPI = module.default;
@@ -32,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if (input.files && input.files[0]) {
       var reader = new FileReader();
 
-      reader.onload = function (e) {
+      reader.onload = function(e) {
         $('#preview-image').attr('src', e.target.result);
       }
 
@@ -52,8 +50,39 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
-  // ここでjQueryのオブジェクトに対して$.eachを使用します。
+  // ここでjQueryのオブジェクトに対して.eachを使用します。
   $('[data-refresh-csrf]').each(function() {
     var element = $(this);
+  });
+
+  // カスタム削除確認ダイアログ
+  $('a[data-method="delete"]').each(function(index, element) {
+    $(element).on('click', function(event) {
+      event.preventDefault();
+      if (confirm('本当によろしいですか？')) {
+        var form = document.createElement('form');
+        form.method = 'POST';
+        form.action = this.href;
+
+        var hiddenField = document.createElement('input');
+        hiddenField.type = 'hidden';
+        hiddenField.name = '_method';
+        hiddenField.value = 'delete';
+
+        form.appendChild(hiddenField);
+
+        var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+        var csrfField = document.createElement('input');
+        csrfField.type = 'hidden';
+        csrfField.name = 'authenticity_token';
+        csrfField.value = csrfToken;
+
+        form.appendChild(csrfField);
+        document.body.appendChild(form);
+        form.submit();
+      } else {
+        return false;
+      }
+    });
   });
 });
