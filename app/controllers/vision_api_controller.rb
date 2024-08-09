ce991df361120da72f28edbd628552314ffcdfef
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-08-10 07:58:14 +0900

    vision_api rubocop訂正

--- app/controllers/vision_api_controller.rb
+++ app/controllers/vision_api_controller.rb
@@ -8,51 +8,93 @@ class VisionApiController < ApplicationController
   require 'json'
 
   def upload
-    credentials_hash = JSON.parse(Rails.application.credentials.dig(:google_cloud, :credentials))
+    credentials_hash = google_credentials
 
-    # Google Cloud Vision APIクライアントを初期化
-    image_annotator_client = Google::Cloud::Vision.image_annotator do |config|
-      config.credentials = credentials_hash
-    end
+    image_annotator_client = initialize_image_annotator_client(credentials_hash)
+    image_path = params[:image].path
 
-    # 画像のパスを取得
-    image = params[:image].path
+    response = perform_label_detection(image_annotator_client, image_path)
 
-    # 画像のラベル検出を実行
-    response = image_annotator_client.label_detection(image:)
-
-    # レスポンスから最初のラベルの説明を取得
-    if response.responses.any? && response.responses.first.label_annotations.any?
-      first_label = response.responses.first.label_annotations.first.description
-
-      # 翻訳を実行
-      translated_label = translate_text(first_label)
-
-      @label_data = { translated_name: translated_label }
-    end
-
-    # JSON形式でレスポンスを返す
-    respond_to do |format|
-      format.json { render json: @label_data }
+    if valid_response?(response)
+      translated_label = translate_first_label(response)
+      render_json_response(translated_label)
+    else
+      render_empty_response
     end
   end
 
   private
 
+  def google_credentials
+    JSON.parse(Rails.application.credentials.dig(:google_cloud, :credentials))
+  end
+
+  def initialize_image_annotator_client(credentials_hash)
+    Google::Cloud::Vision.image_annotator do |config|
+      config.credentials = credentials_hash
+    end
+  end
+
+  def perform_label_detection(image_annotator_client, image_path)
+    image_annotator_client.label_detection(image: image_path)
+  end
+
+  def valid_response?(response)
+    response.responses.any? && response.responses.first.label_annotations.any?
+  end
+
+  def translate_first_label(response)
+    first_label = response.responses.first.label_annotations.first.description
+    translate_text(first_label)
+  end
+
+  def render_json_response(translated_label)
+    @label_data = { translated_name: translated_label }
+    respond_to do |format|
+      format.json { render json: @label_data }
+    end
+  end
+
+  def render_empty_response
+    respond_to do |format|
+      format.json { render json: {} }
+    end
+  end
+
   def translate_text(text)
-    api_key = Rails.application.credentials.dig(:deepl, :api_key)
-    url = URI.parse('https://api-free.deepl.com/v2/translate')
+    api_key = fetch_deepl_api_key
+    url = build_deepl_url
+    request = build_deepl_request(url, text, api_key)
+
+    response = send_deepl_request(url, request)
+    parse_translation_response(response)
+  end
+
+  def fetch_deepl_api_key
+    Rails.application.credentials.dig(:deepl, :api_key)
+  end
+
+  def build_deepl_url
+    URI.parse('https://api-free.deepl.com/v2/translate')
+  end
+
+  def build_deepl_request(url, text, api_key)
     request = Net::HTTP::Post.new(url.to_s)
     request['Authorization'] = "DeepL-Auth-Key #{api_key}"
     request.set_form_data({
                             'text' => text,
                             'target_lang' => 'JA'
                           })
+    request
+  end
 
-    response = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
+  def send_deepl_request(url, request)
+    Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
       http.request(request)
     end
+  end
 
+  def parse_translation_response(response)
     result = JSON.parse(response.body)
     result['translations'].first['text'] if result['translations'].any?
   end
