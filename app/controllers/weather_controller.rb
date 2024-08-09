a3c0d2482e3949130fb4e57e08190df246516d23
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-08-10 08:55:55 +0900

    天気途中x”

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,6 +2,8 @@
 
 # WeatherController manages weather-related actions and views.
 class WeatherController < ApplicationController
+  include WeatherHelper
+
   before_action :validate_city, only: [:show]
   before_action :load_prefecture_data, only: [:index]
   before_action :authenticate_user!
@@ -26,48 +28,46 @@ class WeatherController < ApplicationController
     @weather_data = fetch_weather_service_data(params[:city] || default_city)
 
     if @weather_data
-      rainfall = @weather_data.dig('rain', '1h') || @weather_data.dig('rain', '3h') || 0
-      @weather = extract_weather_data(@weather_data, rainfall)
-      fetch_and_update_weather_forecast(params[:city] || default_city)
-
-      send_line_notification("【#{@weather[:name]}】に降水が予測されています。傘をお持ちください。") if rainfall.positive?
+      process_weather_data(@weather_data, params[:city] || default_city)
     else
-      flash[:alert] = '天気情報の取得に失敗しました。'
+      flash[:alert] = I18n.t('weather.fetch_failed')
       redirect_to action: :index
     end
   end
 
   def map
     authorize :weather, :map?
-    prefecture_ids = [1, 4, 13, 23, 15, 26, 34, 39, 40, 46, 47]
-    prefectures = Prefecture.where(id: prefecture_ids)
-
-    @weather_data = prefectures.map do |prefecture|
-      weather = fetch_weather_api_data(prefecture.name)
-      forecasts = if weather.present? && weather['list'].present?
-                    weather['list'].select do |forecast|
-                      Time.at(forecast['dt']) <= Time.now + 24.hours
-                    end
-                  else
-                    []
-                  end
-      { name: prefecture.name, weather:, forecasts: }
-    end
-
-    return unless @weather_data.any? { |data| data[:weather].nil? }
-
-    flash.now[:alert] = '一部の天気情報の取得に失敗しました。'
+    @weather_data = fetch_weather_for_prefectures
+    flash_part_weather_fetch_warning if weather_fetch_failed?
   end
 
   private
 
   def validate_city
-    redirect_to(action: :index, alert: '都市名を入力してください。') if params[:city].blank?
+    redirect_to(action: :index, alert: I18n.t('weather.enter_city')) if params[:city].blank?
   end
 
   def load_prefecture_data
     csv_path = Rails.root.join('prefectures.csv')
     @prefecture_data = CSV.read(csv_path, headers: true).map { |row| [row['prefecture'], row['english']] }
+  end
+
+  def fetch_weather_data_for_user(user)
+    default_city = fetch_city_name(user&.prefecture_id, 'Tokyo')
+    second_city = fetch_city_name(user&.second_prefecture_id, 'Osaka')
+
+    {
+      default_city: fetch_weather_api_data(default_city),
+      second_city: fetch_weather_api_data(second_city)
+    }
+  end
+
+  def fetch_city_name(prefecture_id, default_city)
+    prefecture_id.present? ? Prefecture.find(prefecture_id).name : default_city
+  end
+
+  def flash_weather_fetch_warning
+    flash.now[:alert] = I18n.t('weather.fetch_failed')
   end
 
   def fetch_weather_service_data(city)
@@ -79,28 +79,16 @@ class WeatherController < ApplicationController
     api_key = Rails.application.credentials.dig(:openweathermap, :api_key)
     response = HTTParty.get('https://api.openweathermap.org/data/2.5/forecast',
                             query: { q: city_name, appid: api_key, units: 'metric', lang: 'ja' })
-    if response.success?
-      response.parsed_response
-    else
-      Rails.logger.error "Failed to fetch weather data for #{city_name}: #{response.message}"
-      nil
-    end
+    return response.parsed_response if response.success?
+
+    nil
   end
 
-  def fetch_temperature(location); end
-
-  def extract_weather_data(weather_data, rainfall)
-    {
-      name: weather_data['name'],
-      temp_celsius: kelvin_to_celsius(weather_data['main']['temp']).round(1),
-      feels_like_celsius: kelvin_to_celsius(weather_data['main']['feels_like']).round(1),
-      temp_min_celsius: kelvin_to_celsius(weather_data['main']['temp_min']).round(1),
-      temp_max_celsius: kelvin_to_celsius(weather_data['main']['temp_max']).round(1),
-      humidity: weather_data['main']['humidity'],
-      wind_speed: weather_data['wind']['speed'],
-      description: weather_data['weather'][0]['description'],
-      rainfall:
-    }
+  def process_weather_data(weather_data, city)
+    rainfall = fetch_rainfall(weather_data)
+    @weather = build_weather_data(weather_data, rainfall)
+    fetch_and_update_weather_forecast(city)
+    send_rainfall_notification(rainfall) if rainfall.positive?
   end
 
   def fetch_and_update_weather_forecast(city)
@@ -108,11 +96,44 @@ class WeatherController < ApplicationController
     @weather_data = response_forecast.parsed_response if response_forecast.success?
   end
 
-  def kelvin_to_celsius(kelvin)
-    kelvin - 273.15
+  def fetch_rainfall(weather_data)
+    weather_data.dig('rain', '1h') || weather_data.dig('rain', '3h') || 0
   end
 
-  def send_line_notification(message)
-    # LINE通知を送信するコードをここに追加します
+  def send_rainfall_notification(rainfall)
+    send_line_notification(I18n.t('weather.rainfall_alert', city: @weather[:name])) if rainfall.positive?
+  end
+
+  def fetch_weather_for_prefectures
+    prefecture_ids = [1, 4, 13, 23, 15, 26, 34, 39, 40, 46, 47]
+    Prefecture.where(id: prefecture_ids).map do |prefecture|
+      fetch_prefecture_weather(prefecture)
+    end
+  end
+
+  def fetch_prefecture_weather(prefecture)
+    weather = fetch_weather_api_data(prefecture.name)
+    forecasts = select_forecasts(weather)
+    { name: prefecture.name, weather:, forecasts: }
+  end
+
+  def select_forecasts(weather)
+    return [] unless weather.present? && weather['list'].present?
+
+    weather['list'].select do |forecast|
+      forecast_within_24_hours?(forecast['dt'])
+    end
+  end
+
+  def forecast_within_24_hours?(timestamp)
+    Time.zone.at(timestamp) <= 24.hours.from_now
+  end
+
+  def weather_fetch_failed?
+    @weather_data.any? { |data| data[:weather].nil? }
+  end
+
+  def flash_part_weather_fetch_warning
+    flash.now[:alert] = I18n.t('weather.partial_fetch_failed')
   end
 end
--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -1,4 +1,41 @@
-# frozen_string_literal: true
+# app/helpers/weather_helper.rb
 
 module WeatherHelper
+  def build_weather_data(weather_data, rainfall)
+    {
+      name: extract_weather_name(weather_data),
+      temp_celsius: extract_temperature(weather_data, 'temp'),
+      feels_like_celsius: extract_temperature(weather_data, 'feels_like'),
+      temp_min_celsius: extract_temperature(weather_data, 'temp_min'),
+      temp_max_celsius: extract_temperature(weather_data, 'temp_max'),
+      humidity: extract_humidity(weather_data),
+      wind_speed: extract_wind_speed(weather_data),
+      description: extract_description(weather_data),
+      rainfall: 
+    }
+  end
+
+  def extract_weather_name(weather_data)
+    weather_data['name']
+  end
+
+  def extract_temperature(weather_data, key)
+    kelvin_to_celsius(weather_data['main'][key]).round(1)
+  end
+
+  def extract_humidity(weather_data)
+    weather_data['main']['humidity']
+  end
+
+  def extract_wind_speed(weather_data)
+    weather_data['wind']['speed']
+  end
+
+  def extract_description(weather_data)
+    weather_data['weather'][0]['description']
+  end
+
+  def kelvin_to_celsius(kelvin)
+    kelvin - 273.15
+  end
 end
