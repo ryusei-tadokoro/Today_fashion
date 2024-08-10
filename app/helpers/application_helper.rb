73b13041b6987927e5f52852c913b0e424def9e8
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-08-10 13:05:19 +0900

    一部修正”

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,7 @@ module ApplicationHelper
   private
 
   def default_meta_tags
-    {
-      site: 'TodayFashion',
+    { site: 'TodayFashion',
       title: '天気予報や体質による服装決めとファッションアイテム管理サービス',
       reverse: true,
       charset: 'utf-8',
@@ -18,21 +17,27 @@ module ApplicationHelper
       keywords: '服,天気',
       canonical: request.original_url,
       separator: '|',
-      og: {
-        site_name: :site,
-        title: :title,
-        description: :description,
-        type: 'website',
-        url: request.original_url,
-        image: image_url('todayfashion-icon.png'), # 配置するパスやファイル名によって変更すること
-        local: 'ja-JP'
-      },
-      # Twitter用の設定を個別で設定する
-      twitter: {
-        card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
-        site: '@todayfashion514', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
-        image: image_url('todayfashion-icon.png') # 配置するパスやファイル名によって変更すること
-      }
+      og: og_meta_tags,
+      twitter: twitter_meta_tags }
+  end
+
+  def og_meta_tags
+    {
+      site_name: :site,
+      title: :title,
+      description: :description,
+      type: 'website',
+      url: request.original_url,
+      image: image_url('todayfashion-icon.png'), # 配置するパスやファイル名によって変更すること
+      local: 'ja-JP'
+    }
+  end
+
+  def twitter_meta_tags
+    {
+      card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
+      site: '@todayfashion514', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
+      image: image_url('todayfashion-icon.png') # 配置するパスやファイル名によって変更すること
     }
   end
 end
--- app/jobs/push_line_job.rb
+++ app/jobs/push_line_job.rb
@@ -7,25 +7,33 @@ class PushLineJob < ApplicationJob
 
   def perform(*_args)
     User.find_each do |user|
-      # ユーザーのデフォルト都市とセカンド都市を取得
-      default_city = user.prefecture_id? ? Prefecture.find(user.prefecture_id).name : 'Tokyo'
-      second_city = user.second_prefecture_id? ? Prefecture.find(user.second_prefecture_id).name : 'Osaka'
-
-      # 両都市の天気予報を取得し、必要に応じて通知を送信
-      [default_city, second_city].each do |city|
+      cities = fetch_user_cities(user)
+      cities.each do |city|
         weather_data = fetch_weather_data(city)
-        next unless weather_data
-
-        rainfall = weather_data.dig('rain', '1h') || weather_data.dig('rain', '3h') || 0
-        if rainfall.positive?
-          message = "【#{city}】に降水が予測されています。傘をお持ちください。"
-          send_line_notification(user.uid, message)
-        end
+        send_rainfall_notification(user, city, weather_data) if weather_data
       end
     end
   end
 
   private
+
+  def fetch_user_cities(user)
+    default_city = user.prefecture_id? ? Prefecture.find(user.prefecture_id).name : 'Tokyo'
+    second_city = user.second_prefecture_id? ? Prefecture.find(user.second_prefecture_id).name : 'Osaka'
+    [default_city, second_city]
+  end
+
+  def send_rainfall_notification(user, city, weather_data)
+    rainfall = fetch_rainfall(weather_data)
+    return unless rainfall.positive?
+
+    message = "【#{city}】に降水が予測されています。傘をお持ちください。"
+    send_line_notification(user.uid, message)
+  end
+
+  def fetch_rainfall(weather_data)
+    weather_data.dig('rain', '1h') || weather_data.dig('rain', '3h') || 0
+  end
 
   def fetch_weather_data(city)
     response = WeatherService.new(city).fetch_weather
