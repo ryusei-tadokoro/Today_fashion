77a5447a62ea069a58afa197b1ce07f73096c450
ryusei-tadokoro <ryusei.tadokoro.0129@gmail.com>
2024-10-01 07:34:48 +0900

    修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+set :output, "log/cron.log"
+set :environment, 'production'
+
 every 1.day, at: '6:00 am' do
-  runner 'PushLineJob.perform_later'
+  runner "PushLineJob.perform_later"
 end
